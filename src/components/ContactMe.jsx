7e46cf03426a0e76d823485b87c8fdd0ebcb2e2e
Akinur Rahman <akinurrahmanofficial@gmail.com>
2023-09-11 22:31:01 +0530

    now user can contact

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,39 +1,59 @@
-import React from 'react'
+import React from "react";
 
 const ContactMe = () => {
   return (
     <>
-    <section className="contact" id="contact">
-      <h2 className="heading">Contact <span>Me</span></h2>
+      <section className="contact" id="contact">
+        <h2 className="heading">
+          Contact <span>Me</span>
+        </h2>
 
-      <form>
-        <div className="input-box">
-          <input type="text" placeholder="Full Name" />
-          <input type="email" placeholder="Email Address" />
-        </div>
-        <div className="input-box">
-          <input type="number" placeholder="Mobile Number" />
-          <input type="text" placeholder="Email Subject" />
-        </div>
-        <textarea
-          name=""
-          id=""
-          cols="30"
-          rows="10"
-          placeholder="Your messege"
-        ></textarea>
-        <input type="submit" value="Send Message" className="btn" onClick={()=> alert(`Sorry! you can't send message`)} />
-      </form>
-    </section>
-
+        <form method="POST" action="https://formspree.io/f/mzblvpdq">
+          <div className="input-box">
+            <input
+              required
+              type="text"
+              placeholder="Full Name"
+              autoComplete="off"
+              name="name"
+            />
+            <input
+              required
+              type="email"
+              placeholder="Email Address"
+              autoComplete="off"
+              name="email"
+            />
+          </div>
+          <div className="input-box">
+            <input
+              required
+              type="number"
+              placeholder="Mobile Number"
+              autoComplete="off"
+              name="number"
+            />
+            <input
+              required
+              type="text"
+              placeholder="Email Subject"
+              autoComplete="off"
+              name="subject"
+            />
+          </div>
+          <textarea
+            required
+            name="email body"
+            cols="30"
+            rows="10"
+            placeholder="Your messege"
+            autoComplete="off"
+          ></textarea>
+          <input type="submit" value="Send Message" className="btn" />
+        </form>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default ContactMe
-
-// const submitBtn = document.querySelector('[value="Send Message"]')
-// submitBtn.addEventListener('click',()=>{
-// alert("Sorry! Messege cann't sent")
-
-// })
+export default ContactMe;
